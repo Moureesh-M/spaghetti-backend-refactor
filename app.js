d39bf0cff8afcfb707b4522018cbb6b313cd0204
Moureesh <mmoureesh@gmail.com>
2026-04-25 12:12:16 +0530

    Fixed handlers

--- app.js
+++ app.js
@@ -6,7 +6,27 @@ const app = express();
 
 app.use(express.json());
 
+app.get(['/', '/get'], (req, res) => {
+	res.json({
+		message: 'API is running',
+		routes: [
+			'GET /users',
+			'GET /users/:id',
+			'POST /users',
+			'DELETE /users/:id',
+			'GET /posts',
+			'GET /posts/:id',
+			'POST /posts',
+			'PATCH /posts/:id/publish',
+		],
+	});
+});
+
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
+
+app.use((req, res) => {
+	res.status(404).json({ error: `Route not found: ${req.method} ${req.originalUrl}` });
+});
 
 app.listen(3000, () => console.log('Server running on port 3000'));
